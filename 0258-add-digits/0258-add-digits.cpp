b04e54cf6c516daad3349a93dc4fe182eaee8add
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2026-07-06 17:58:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.2%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while (num > 9) {
-            int sum = 0;
-
-            while (num != 0) {
-                int dig = num % 10;
-                sum += dig;
-                num /= 10;
-            }
-            num = sum;
-        }
-
-        return num;
+        int sum = 1 + (num - 1) % 9;
+        return sum;
     }
 };
