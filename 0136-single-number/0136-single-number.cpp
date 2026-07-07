994214438a5f9219b58b60c1077757f78b36cc25
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2026-07-07 22:23:23 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.6%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int num = 0;
+        for(int val : nums) {
+            num ^= val;
+        }
+
+        return num;
+    }
+};
