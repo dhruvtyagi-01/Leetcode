3fa8b485a45c9070c138376bfbf3e8f48b0b11d9
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2026-07-10 21:58:45 +0530

    Time: 2 ms (41.35%), Space: 10.3 MB (74.84%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+
+    string reverseVowels(string s) {
+        int n = s.length();
+        int start = 0, end = n - 1;
+
+        while (start < end) {
+            if(isVowel(s[start]) && isVowel(s[end])) {
+                swap(s[start], s[end]);
+                start++;
+                end--;
+            } else if(!isVowel(s[start])) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return s;
+    }
+};
