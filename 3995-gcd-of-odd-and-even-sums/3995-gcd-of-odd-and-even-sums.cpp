cc83cd0ee23d1aca919dd2abec747ab2a81027ab
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2026-07-17 17:06:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (81.39%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int oddSum = n * n;
-        int evenSum = n * (n + 1);
-
-        while (oddSum != 0) {
-            int temp = oddSum;
-            oddSum = evenSum % oddSum;
-            evenSum = temp;
-        }
-        return evenSum;
+        return n;   
     }
 };
