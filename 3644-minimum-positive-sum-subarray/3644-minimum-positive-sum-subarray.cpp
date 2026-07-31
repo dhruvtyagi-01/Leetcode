de0f519b008ae4c91f6e747571dce890f164c5f9
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2026-07-31 09:16:14 +0530

    Time: 0 ms (100%), Space: 29.4 MB (67.16%) - LeetHub

--- 3644-minimum-positive-sum-subarray/3644-minimum-positive-sum-subarray.cpp
+++ 3644-minimum-positive-sum-subarray/3644-minimum-positive-sum-subarray.cpp
@@ -3,7 +3,6 @@ public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
         int n = nums.size();
         int ans = INT_MAX;
-
         for (int i = 0; i < n; i++) {
             int sum = 0;
 
